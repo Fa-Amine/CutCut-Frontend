703a5bc9a0d246f4d0176628e42ac604815cd55a
Houssam <houssamahmichs@gmail.com>
2026-05-23 16:41:29 +0100

    remove dark mode button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal, effect } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
@@ -10,27 +10,4 @@ import { CommonModule } from '@angular/common';
 })
 export class AppComponent {
   title = 'barber-web-app';
-  isDarkMode = signal(false);
-
-  constructor() {
-    const saved = localStorage.getItem('darkMode');
-    if (saved === 'true') {
-      this.isDarkMode.set(true);
-      document.body.classList.add('dark-mode');
-    }
-
-    effect(() => {
-      if (this.isDarkMode()) {
-        document.body.classList.add('dark-mode');
-        localStorage.setItem('darkMode', 'true');
-      } else {
-        document.body.classList.remove('dark-mode');
-        localStorage.setItem('darkMode', 'false');
-      }
-    });
-  }
-
-  toggleDarkMode() {
-    this.isDarkMode.update(v => !v);
-  }
 }
