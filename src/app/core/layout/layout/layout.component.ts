ab61fb225ab57cabd31219c2003dc133f4e755e0
Houssam <houssamahmichs@gmail.com>
2026-05-23 17:24:00 +0100

    add footer

--- src/app/core/layout/layout/layout.component.ts
+++ src/app/core/layout/layout/layout.component.ts
@@ -2,13 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from '../../../shared/components/navbar/navbar.component';
 import { BottomNavbarComponent } from '../../../shared/components/bottom-navbar/bottom-navbar.component';
+import { FooterComponent } from '../../../shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, BottomNavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, BottomNavbarComponent, FooterComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
-export class LayoutComponent {
-}
+export class LayoutComponent {}
