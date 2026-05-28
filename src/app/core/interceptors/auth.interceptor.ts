8048d835754627e106ed5f8ea3266c8477c0d060
Houssam <houssamahmichs@gmail.com>
2026-05-28 14:51:02 +0100

    fix auth interceptor and show real errors

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -2,20 +2,11 @@ import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { SessionService } from '../services/session.service';
 
-/*export const authInterceptor: HttpInterceptorFn = (req, next) => {
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const sessionService = inject(SessionService);
-  const token = sessionService.token();
+  const token = sessionService.user()?.token;
 
-  // Skip adding token if none exists
-  if (!token) {
-    return next(req);
-  }
-
-  // Optional: skip auth header for external/public assets
-  if (
-    req.url.startsWith('/assets') ||
-    req.url.startsWith('assets/')
-  ) {
+  if (!token || req.url.startsWith('/assets')) {
     return next(req);
   }
 
@@ -24,6 +15,5 @@ import { SessionService } from '../services/session.service';
       Authorization: `Bearer ${token}`
     }
   });
-
   return next(authReq);
-};*/
+};
